d6c30ec9193e7fa82bcbee6fcaec6ec1d028315e
mdtang <mdtang@umich.edu>
2016-11-24 03:02:52 -0500

    change enemy speed and added gun,enemy, and bg sounds

--- pygamecode.py
+++ pygamecode.py
@@ -2,6 +2,8 @@ import pygame
 import time
 import random
 from pygame.locals import *
+
+pygame.mixer.init()
 pygame.init()
 
 white = (255, 255, 255)
@@ -17,6 +19,13 @@ clock = pygame.time.Clock()
 # loading and resizing background image
 bg = pygame.image.load('dungeon.bmp')
 bg = pygame.transform.scale(bg,(screen_width,screen_height))
+
+def display_text(surface, text, size, x, y):
+    font = pygame.font.Font(None, size)
+    text_surface = font.render(text, True, white)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = x, y
+    surface.blit(text_surface, text_rect)
 
 
 class PlayerSprite(pygame.sprite.Sprite):
@@ -50,9 +59,9 @@ class PlayerSprite(pygame.sprite.Sprite):
 
         keys = pygame.key.get_pressed()
 
-        if keys[K_a]:
+        if keys[K_LEFT]:
             self.speedX = -20
-        elif keys[K_d]:
+        elif keys[K_RIGHT]:
             self.speedX = 20
         self.rect.x += self.speedX
         
@@ -70,6 +79,9 @@ class PlayerSprite(pygame.sprite.Sprite):
         allsprites.add(bullet)
 
         bullets.add(bullet)
+
+        #adding gun sound only when prompted
+        gun_sound.play()
                 
 
 class EnemySprite(pygame.sprite.Sprite):
@@ -78,7 +90,8 @@ class EnemySprite(pygame.sprite.Sprite):
 
         self.enemywidth = 150
         self.enemyheight = 135
-
+        
+        # randomly choose enemy images
         self.image = random.choice(enemyimages)
 
         self.image = pygame.transform.scale(self.image, (self.enemywidth,self.enemyheight))
@@ -91,16 +104,20 @@ class EnemySprite(pygame.sprite.Sprite):
         # declare variables for random positions for enemy sprites
         self.rect.x = random.randrange(screen_width - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedY = random.randrange(1,10)
+
+        self.speedX = random.randrange(-2,2)
+        self.speedY = random.randrange(5,10)
 
     def update(self):
         self.rect.y += self.speedY
+        self.rect.x += self.speedX
         if self.rect.top > screen_height + 10:
             # Spawning enemies from different x coordinates so they don't come from the same place
             self.rect.x = random.randrange(screen_width - self.rect.width)
             # Spawning enemies off screen at random y coordinates
             self.rect.y = random.randrange(-200, -80)
             # varying speeds for enemy sprites
+            self.speedX = random.randrange(-2,2)
             self.speedY = random.randrange(5,10)
 
 
@@ -130,13 +147,32 @@ class BulletSprite(pygame.sprite.Sprite):
             # removes bullet sprite
             self.kill()
 
+# loading multiple enemy images
 enemyimages = []
 enemylist = ['monster1.gif', 'monster2.gif', 'monster3.gif', 'monster4.gif']
 
 for image in enemylist:
     enemyimages.append(pygame.image.load(image).convert_alpha())
 
-# sprite groups created
+# loading explosion animation images
+expl_anim = []
+
+
+# loading game sounds
+gun_sound = pygame.mixer.Sound("gunsound.wav")
+
+# loading different monster death sounds
+monster_sounds = []
+soundslist = ['enemysound1.wav','enemysound2.wav','enemysound3.wav']
+for sound in soundslist:
+    monster_sounds.append(pygame.mixer.Sound(sound))
+
+# loading continuous background music
+pygame.mixer.music.load('bgmusic.wav')
+# setting volume to 60%
+pygame.mixer.music.set_volume(0.6)
+
+# creating sprite groups
 allsprites = pygame.sprite.Group()
 enemymobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -144,8 +180,8 @@ bullets = pygame.sprite.Group()
 player = PlayerSprite()
 allsprites.add(player)
 
-# creating list of random numbers from 3 to 8 and shuffling for randomness
-enemycount = list(range(3,8))
+# creating list of random numbers from 5 to 15 and shuffling for randomness
+enemycount = list(range(5, 15))
 random.shuffle(enemycount)
 
 for i in enemycount:
@@ -155,13 +191,14 @@ for i in enemycount:
     # adding enemy sprites into sprites group
     enemymobs.add(mob)
 
-#spritesheet = pygame.image.load("spritesheet.bmp")
-#num_images = 10
-#current_imgs = 0
 
 clock.tick(60) # 60 fps cap
 
+# initialize score
 score = 0 
+
+# infinite looping background music
+pygame.mixer.music.play(loops= -1)
 
 gameExit = False
 while not gameExit:
@@ -178,16 +215,16 @@ while not gameExit:
     # check for player-enemy (circle sprites) sprite collision (creates list)
     hits = pygame.sprite.spritecollide(player,enemymobs, False, pygame.sprite.collide_circle)
     if hits:
-        #myFont = pygame.font.Font(None, 100)
-        #label = myFont.render("GAME OVER", 1, (255,255,0))
-        #gameDisplay.blit(label, (220, 250))
         # ends game
         gameExit = True
 
     # check for enemy-bullet sprite collision
     bullethits = pygame.sprite.groupcollide(enemymobs,bullets, True, True)
     for hits in bullethits:
-        #hits += 1
+        # 1 damage per hit
+        score += 100
+        # randomly choose and play enemy sounds
+        random.choice(monster_sounds).play()
         # spawn new enemy mob
         mob = EnemySprite()
         allsprites.add(mob)
@@ -198,6 +235,10 @@ while not gameExit:
     # rendering
     gameDisplay.blit(bg,(0,0))
     allsprites.draw(gameDisplay)
+    # text is score
+    display_text(gameDisplay, str(score), 30, screen_width * .5, 10)
+
+    # displaying
     pygame.display.flip()
 
 #required
